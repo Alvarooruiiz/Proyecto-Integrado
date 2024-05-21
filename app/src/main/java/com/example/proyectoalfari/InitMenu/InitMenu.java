79ba43254e24710c3c4f42bcf3b2cbd307e0ca60
Alvarooruiiz <alvaro.ruiz.enriquez@hotmail.com>
2024-05-21 20:09:03 +0200

    Proyecto Integrado

--- app/src/main/java/com/example/proyectoalfari/InitMenu/InitMenu.java
+++ app/src/main/java/com/example/proyectoalfari/InitMenu/InitMenu.java
@@ -1,6 +1,7 @@
 package com.example.proyectoalfari.InitMenu;
 
 import android.os.Bundle;
+import android.widget.ImageView;
 
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
@@ -9,10 +10,16 @@ import com.example.proyectoalfari.R;
 
 public class InitMenu extends AppCompatActivity {
 
+    private ImageView ivCartaDish;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.initmenu_layout);
+
+
+        ivCartaDish = findViewById(R.id.ivCartaDish);
+
 
         Toolbar toolbar = findViewById(R.id.topAppBar);
         setSupportActionBar(toolbar);
--- app/src/main/java/com/example/proyectoalfari/Splash/SplashScreen.java
+++ app/src/main/java/com/example/proyectoalfari/Splash/SplashScreen.java
@@ -1,4 +1,30 @@
 package com.example.proyectoalfari.Splash;
 
-public class SplashScreen {
+import android.content.Intent;
+import android.os.Bundle;
+import android.os.Handler;
+
+import androidx.annotation.Nullable;
+import androidx.appcompat.app.AppCompatActivity;
+
+import com.example.proyectoalfari.Login;
+import com.example.proyectoalfari.R;
+
+public class SplashScreen extends AppCompatActivity {
+
+    private static final int SPLASH_SCREEN_DURATION = 3000;
+
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.splash_layout);
+        new Handler().postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                Intent intent = new Intent(SplashScreen.this, Login.class);
+                startActivity(intent);
+                finish();
+            }
+        }, SPLASH_SCREEN_DURATION);
+    }
 }
