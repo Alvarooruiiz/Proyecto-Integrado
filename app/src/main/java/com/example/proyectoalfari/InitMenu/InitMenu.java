1e7d39859611fd928cd516332e5e45d6665ec425
Alvarooruiiz <147401269+Alvarooruiiz@users.noreply.github.com>
2024-05-21 08:42:56 +0200

    Proyecto Integrado

--- app/src/main/java/com/example/proyectoalfari/InitMenu/InitMenu.java
+++ app/src/main/java/com/example/proyectoalfari/InitMenu/InitMenu.java
@@ -1,6 +1,33 @@
 package com.example.proyectoalfari.InitMenu;
 
-public class InitMenu {
+import android.os.Bundle;
 
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
+
+import com.example.proyectoalfari.R;
+
+public class InitMenu extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.initmenu_layout);
+
+        Toolbar toolbar = findViewById(R.id.topAppBar);
+        setSupportActionBar(toolbar);
+
+        toolbar.setNavigationOnClickListener(view -> {
+
+        });
+
+        toolbar.setOnMenuItemClickListener(item -> {
+            if (item.getItemId() == R.id.action_profile) {
+
+                return true;
+            }
+            return false;
+        });
+    }
 
 }
